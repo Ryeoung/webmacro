a2b81999044fbbe32fafa5386d604dc334e26d68
Ryung Hong Lee <koreaso2003@gmail.com>
2021-06-08 19:46:09 +0900

    git selenium hub (docker) 설정 완료

--- src/main/java/com/macro/parking/crawler/PageCrawler.java
+++ src/main/java/com/macro/parking/crawler/PageCrawler.java
@@ -18,17 +18,18 @@ public class PageCrawler {
 	protected WebDriver driver;
 	protected String driverName;
 	protected String driverPath;
-	protected final String HOST_URL = "https://selenium-hub:4444/wd/hub";
+	protected final String HOST_URL = "http://selenium-hub:4444/wd/hub";
 	public void setupChromeDriver()  {
-//	      System.setProperty(driverName, driverPath);
 		WebDriver driver = null;
 
 		try {
 
-//  			WebDriverManager.chromedriver().setup();
-//	  		List<String> list = WebDriverManager.chromedriver().getDriverVersions();
 	  		ChromeOptions options = new ChromeOptions();
 	  		options.addArguments("--window-size=1920,1080");
+	  		//도커 컨테이너에서 크롬 드라이브를 사용 시 /dev/shm 메모리가 부족해서 사용하지 않는 다고 표시
+			options.addArguments("--no-sandbox");
+			options.addArguments("--disable-dev-shm-usage");
+
 //	  		options.addArguments("--headless");
 //	  		options.addArguments("--disable-gpu"); // gpu 가속 끄
 //			options.addArguments("User-Agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4044.122 Safari/537.36");
