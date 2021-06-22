872f218f8364203a5c7bf66678ce83f48ff99a34
Ryung Hong Lee <koreaso2003@gmail.com>
2021-06-22 18:08:24 +0900

    docker run

--- src/main/java/com/macro/parking/crawler/PageCrawler.java
+++ src/main/java/com/macro/parking/crawler/PageCrawler.java
@@ -23,7 +23,7 @@ public class PageCrawler {
 		WebDriver driver = null;
 
 		try {
-			WebDriverManager.chromedriver().setup();
+//			WebDriverManager.chromedriver().setup();
 	  		ChromeOptions options = new ChromeOptions();
 	  		options.addArguments("--window-size=1920,1080");
 	  		//도커 컨테이너에서 크롬 드라이브를 사용 시 /dev/shm 메모리가 부족해서 사용하지 않는 다고 표시
@@ -38,8 +38,8 @@ public class PageCrawler {
 			options.setPageLoadStrategy(PageLoadStrategy.EAGER);
 
 
-			driver = new ChromeDriver(options);
-//			driver = new RemoteWebDriver(new URL(HOST_URL), options);
+//			driver = new ChromeDriver(options);
+			driver = new RemoteWebDriver(new URL(HOST_URL), options);
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
